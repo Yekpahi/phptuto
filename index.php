dc3b489f52c02085176471ab021e433ec47cc78d
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-08-04 15:47:03 +0200

    function

--- index.php
+++ index.php
@@ -1,59 +1,27 @@
 <?php
-/*
-$scores = [1, 2, 3];
-$scores[] = 10;
-$scores[5] = 12;
-$scores[4] = 10;
-unset($scores[5]);
-echo '<pre>';
-echo $scores[5];
-echo '</pre>';
-*/
-/*
-$asstab = [];
 
-$asstab = [
-    'nom' => "Yekpahi",
-    'prenom' => "Prince"
-];
+class Customer
+{
+	private $name;
+	public $age;
 
-foreach ($asstab as $as) {
-    echo $as . '<br>';
+
+	public function __construct ($name, $age)
+	{
+		$this->age = $age;
+		$this->name = $name;
+	}
+
+	public function setName($name) {
+		$this->name = $name;
+	}
+	
+	public function getName(){
+		return $this->name;
+	}
 }
-*/
-/*
-$capitals = [
-	'Japan' => 'Tokyo',
-	'France' => 'Paris',
-	'Germany' => 'Berlin',
-	'United Kingdom' => 'London',
-	'United States' => 'Washington D.C.'
-];
 
-foreach ($capitals as $country => $capital) {
-	echo "The capital city of {$country} is $capital" . '<br>';
-}
-*/
+$account = new Customer("Prince", 36);
+echo $account->getName(); // 100
 
-/*
-$user = [
-	'username' => 'admin',
-	'email' => 'admin@phptutorial.net',
-	'is_active' => '1'
-];
 
-$properties = array_keys($user, 'admin');
-
-print_r($properties);
-
-*/
-
-$message = 'Hi';
-$say = function () use ($message) {
-	$message = 'Hello';
-	echo $message;
-};
-
-$say();
-echo '<br>';
-echo $message;
