84c237b2452b6d8f7e52c3dca9cfc76bc30a3035
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-08-04 13:26:14 +0200

    function

--- index.php
+++ index.php
@@ -1,18 +1,59 @@
-<?php 
+<?php
 /*
-$counter = 1;
-function increase($value)
-{
-	$value+= 1;
-	echo $value;// 2
-}
+$scores = [1, 2, 3];
+$scores[] = 10;
+$scores[5] = 12;
+$scores[4] = 10;
+unset($scores[5]);
+echo '<pre>';
+echo $scores[5];
+echo '</pre>';
+*/
+/*
+$asstab = [];
 
+$asstab = [
+    'nom' => "Yekpahi",
+    'prenom' => "Prince"
+];
+
+foreach ($asstab as $as) {
+    echo $as . '<br>';
+}
+*/
+/*
+$capitals = [
+	'Japan' => 'Tokyo',
+	'France' => 'Paris',
+	'Germany' => 'Berlin',
+	'United Kingdom' => 'London',
+	'United States' => 'Washington D.C.'
+];
+
+foreach ($capitals as $country => $capital) {
+	echo "The capital city of {$country} is $capital" . '<br>';
+}
+*/
+
+/*
+$user = [
+	'username' => 'admin',
+	'email' => 'admin@phptutorial.net',
+	'is_active' => '1'
+];
+
+$properties = array_keys($user, 'admin');
+
+print_r($properties);
 
 */
-$message = "bonjour";
-function say() {
-    $message = "Salut";
-    echo $message;
-}
-require 'view.php';
-?>
+
+$message = 'Hi';
+$say = function () use ($message) {
+	$message = 'Hello';
+	echo $message;
+};
+
+$say();
+echo '<br>';
+echo $message;
